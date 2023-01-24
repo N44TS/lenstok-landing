77108598db79a78a51049304397a56eadcc95cbb
Natalie <natalieg@Natalies-MacBook-Air-3.local>
2023-01-24 15:38:11 +0200

    create landing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Question from "./FAQS/Question";
+import data from "./FAQS/data";
+import VID from "./@lenstokxyz.mp4";
+import POAP from "./poapicon.png";
 
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <headerup>
+          <p>Currently in alpha, still lots of bugs.. pls don't hate us 🙏🌱</p>
+        </headerup>
+
+        <div id="icon-container">
+          <span id="lens-follow-icon" data-handle="lenstokxyz" />
+          <a href="https://twitter.com/lenstokxyz" className="fa fa-twitter" target="_blank" rel="noreferrer noopener" aria-hidden={true} />
+          <a href="https://app.poap.xyz/">
+            <img src={POAP} alt="poapicon" className="poap"></img>
+          </a>
+        </div>
+
+        <h1 className="gradient-text">LensTok</h1>
+
+        <div className="toppara">
+          <h3>
+            A video sharing social media platform. tldr: decentralised TikTok
+          </h3>
+          <h3>Share your video content on your own terms!</h3>
+        </div>
+
+        <div className="container">
+          <div className="button-container">
+            <a
+              href="https://lens-do-it.vercel.app/"
+              className="top-button"
+              target="_blank"
+              rel="noreferrer"
+            >
+              1. Get a .test handle
+            </a>
+            <p>↓</p>
+            <a
+              href="https://lenstok-gamma.vercel.app/"
+              className="bottom-button"
+              target="_blank"
+              rel="noreferrer"
+            >
+              2. Lets play with the Dapp!
+            </a>
+          </div>
+        </div>
+
+        <video src={VID} autoplay loop muted width="65%" controls />
+
+        <main>
+          {/* The FAQ bit */}
+          <div className="containerabc" style={{ marginBottom: "0rem" }}>
+            <h3>FAQ's:</h3>
+            <section className="info">
+              {questions.map((question) => (
+                <Question key={question.id} {...question} />
+              ))}
+            </section>
+          </div>
+        </main>
+
+        <footer>
+       <p>@lenstokxyz</p>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/FAQS/data.jsx
@@ -0,0 +1,46 @@
+const questions = [
+    {
+      id: 1,
+      title: 'What is LensTok? I dont get it?',
+      info:
+        'A video sharing, web3 social media dapp. On web (and sortof mobile). View and upload videos, follow and share with friends, interact and get social with likes, comments, mirrors and collects. ',
+    },
+    {
+      id: 2,
+      title: 'Is it free?',
+      info:
+        'Yes, at the moment we are on testnet, so everything on LensTok is currently free on the Polygon Mumbai chain. Once on mainnet, all apart from collects will be free due to gassless transactions on Lens Protocol.',
+    },
+    {
+      id: 3,
+      title: 'How do I sign up?',
+      info:
+        'You do not need to sign up to use LensTok, anyone can access and view videos. However to interact (like, comment, upload etc) you will need to get a .test handle to be able to log in.',
+    },
+    {
+      id: 4,
+      title: 'Is it only web based? I only have mobile',
+      info:
+        'It\'s a web app that has been optimised for mobile, so it will also work on your mobile device, just not as good - so videos dont autoplay on iphone for example',
+    },
+    {
+      id: 5,
+      title: 'WTF is Lens Protocol?',
+      info:
+        'Lens Protocol is a user-owned, open social graph that any application can plug into, LensTok have built ontop of it. With Lens Protocol, you are in control. You own your profile, where you use it, how you use it, and even how you monetize it. Read more here: https://www.lens.xyz/',
+    },
+    {
+      id: 6,
+      title: 'Why do I have to fund bundlr to upload, that sucks',
+      info:
+        'We know, web3 UX still has a lot to be desired. We used bundlr as they promise superfast uploads. We tested it without and videos took forever! Likely will add option for ipfs upload a la LensTube to give you options',
+   },
+    {
+      id: 7,
+      title: 'How do I get the POAP?',
+      info:
+        'Anyone that gives us feedback via our form during the Buildspace N&W demoday gets a special POAP. Just click the poap icon to be taken to the form.',
+    },
+  ]
+  export default questions
+  
